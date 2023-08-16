938c08b0f6d98b7e3c21db31389bfe587058a723
Yathish R <123yathish.r@gmail.com>
2023-08-16 23:19:31 +0530

    feat: updates to secondary contact

--- src/controllers/identityController.ts
+++ src/controllers/identityController.ts
@@ -17,6 +17,8 @@ export class IdentityController {
 
         // Get the contacts from the database
         const { contactsData, error } = await this.identityQuery.getContacts(phoneNumber, email);
+
+        console.log(contactsData)
         if (error) throw error;
 
         // If no contacts are found, add a primary contact
@@ -24,6 +26,25 @@ export class IdentityController {
             const { primaryContactData, error } = await this.identityQuery.addPrimaryContact(phoneNumber, email);
             if (error) throw error;
             return res.status(200).send({ contact: transformContactResponse(primaryContactData as Contact[]) });
+        }
+
+        const matchedByPhoneNumber = contactsData.find(contact => contact.phone_number === phoneNumber && contact.link_precedence === LinkPrecedence.PRIMARY);
+        const matchedByEmail = contactsData.find(contact => contact.email === email && contact.link_precedence === LinkPrecedence.PRIMARY);
+
+        if (matchedByPhoneNumber && matchedByEmail && matchedByPhoneNumber.id !== matchedByEmail.id) {
+            const olderContact = (new Date(matchedByPhoneNumber.createdAt) < new Date(matchedByEmail.createdAt)) ? matchedByPhoneNumber : matchedByEmail;
+            const newerContact = olderContact.id === matchedByPhoneNumber.id ? matchedByEmail : matchedByPhoneNumber;
+
+            // Turn newer contact to secondary and link to the older contact
+            const {secondaryContactData , error} = await this.identityQuery.updateContactToSecondary(newerContact.id, olderContact.id);
+            newerContact.linkedId = olderContact.id;
+            newerContact.linkPrecedence = LinkPrecedence.SECONDARY;
+
+            if (error) throw error;
+
+            secondaryContactData && contactsData.push(secondaryContactData[0]);
+
+            return res.status(200).send({ contact: transformContactResponse(contactsData as Contact[]) });
         }
 
         const partialMatchWithPhoneNumber = contactsData.find((contact: Contact) => contact.phone_number === phoneNumber);
@@ -40,9 +61,6 @@ export class IdentityController {
 
             secondaryContactData && contactsData.push(secondaryContactData[0]);
         }
-
-
-
 
         return res.status(200).send({ contact: transformContactResponse(contactsData as Contact[]) });
 
--- src/controllers/query/identityQuery.ts
+++ src/controllers/query/identityQuery.ts
@@ -6,16 +6,19 @@ export class IdentityQuery {
 
     getContacts = async (phoneNumber?: string, email?: string) => { 
         const query = `
-            WITH RECURSIVE linked_contacts AS (
-                SELECT id, phone_number, email, linked_id, link_precedence
-                FROM contact
-                WHERE phone_number = $1 OR email = $2
-                UNION
-                SELECT c.id, c.phone_number, c.email, c.linked_id, c.link_precedence
-                FROM contact c
-                JOIN linked_contacts lc ON c.phone_number = lc.phone_number OR c.email = lc.email
-            )
-            SELECT * FROM linked_contacts;
+        WITH RECURSIVE linked_contacts AS (
+            SELECT id, phone_number, email, linked_id, link_precedence, created_at, updated_at
+            FROM contact
+            WHERE phone_number = $1 OR email = $2
+        
+            UNION
+        
+            SELECT c.id, c.phone_number, c.email, c.linked_id, c.link_precedence, c.created_at, c.updated_at
+            FROM contact c
+            JOIN linked_contacts lc ON c.id = lc.linked_id OR lc.id = c.linked_id
+        )
+        
+        SELECT * FROM linked_contacts;
         `;
     
         const values = [phoneNumber, email];
@@ -56,6 +59,25 @@ export class IdentityQuery {
         `;
     
         const values = [phoneNumber, email, linkedId, 'secondary'];
+        
+        try {
+            const { rows } = await pool.query(query, values);
+            return { secondaryContactData: rows };
+        } catch(error) { 
+            console.log(error);
+            return { error };
+        }
+    }
+
+    updateContactToSecondary = async (contactId: Number, linkedId: Number) => { 
+        const query = `
+            UPDATE contact
+            SET linked_id = $1, link_precedence = $2
+            WHERE id = $3
+            RETURNING *;
+        `;
+    
+        const values = [linkedId, 'secondary', contactId];
         
         try {
             const { rows } = await pool.query(query, values);
